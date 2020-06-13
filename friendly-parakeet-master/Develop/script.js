39d9028607fecfcce89f36cddf3535d20f20de82
londonlast21 <biffisbuffm@gmail.com>
2020-06-12 19:06:34 -0500

    added new generation options

--- friendly-parakeet-master/Develop/script.js
+++ friendly-parakeet-master/Develop/script.js
@@ -12,22 +12,33 @@
  function generatePassword() {
    //ask for length when button clicked
   var length = Number(prompt("Please enter number of characters, from 8 to 128."));
+    
+
+
+
   //ask for character type
-  var charType = prompt("Enter a character type: special, numeric, lower case, or upper case.");
+  var charType = prompt("Enter a character type: special, numeric, lower case, or upper case. If choosing two, use the word 'and' .");
   //generate the password from user input
     var characters = "";
     var charTypeEntry = charType.toLowerCase();
-    if (charTypeEntry === "lower case") {
+    if (charTypeEntry === "lower case" || charTypeEntry === "LOWER CASE") {
 
       characters = "abcdefghijklmnopqrstuvwxyz";
 
-    } else if(charTypeEntry === "upper case") {
+    } else if(charTypeEntry === "upper case" || charTypeEntry === "UPPER CASE") {
       characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    } else if(charTypeEntry === "numeric") {
+    } else if(charTypeEntry === "numeric" || charTypeEntry === "NUMERIC") {
       characters = "0123456789";
-    }else if (charTypeEntry === "special") {
+    } else if (charTypeEntry === "special" || charTypeEntry === "SPECIAL") {
       characters = "!@#$%^&*()";
+    } else if (charTypeEntry === "special and upper case" || charTypeEntry === "SPECIAL AND UPPER CASE" || charTypeEntry === "upper case and special" || charTypeEntry === "UPPER CASE AND SPECIAL") {
+      characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+    } else if (charTypeEntry === "numeric and upper case" || charTypeEntry === "NUMERIC AND UPPER CASE" || charTypeEntry === "upper case and numeric" || charTypeEntry === "UPPER CASE AND NUMERIC") {
+      characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    } else if (charTypeEntry === "special and lower case" || charTypeEntry === "SPECIAL AND LOWER CASE" || charTypeEntry === "lower case and special" || charTypeEntry === "LOWER CASE AND SPECIAL") {
+      characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()"
     }
+    //up to here works
     //return value
     var returnValue = "";
     for (var i = 0; i < length; i++) {
