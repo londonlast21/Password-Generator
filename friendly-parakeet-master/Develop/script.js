83e9c0be83329c4082c5fbfbb2e7a4d42bccff42
londonlast21 <biffisbuffm@gmail.com>
2020-06-12 19:26:50 -0500

    fixed combined generation qualities

--- friendly-parakeet-master/Develop/script.js
+++ friendly-parakeet-master/Develop/script.js
@@ -17,7 +17,7 @@
 
 
   //ask for character type
-  var charType = prompt("Enter a character type: special, numeric, lower case, or upper case. If choosing two, use the word 'and' .");
+  var charType = prompt("Enter a character type: 'special', 'numeric', 'lower case', or 'upper case'. If choosing two, use the word 'and' and include entire term .");
   //generate the password from user input
     var characters = "";
     var charTypeEntry = charType.toLowerCase();
@@ -36,7 +36,13 @@
     } else if (charTypeEntry === "numeric and upper case" || charTypeEntry === "NUMERIC AND UPPER CASE" || charTypeEntry === "upper case and numeric" || charTypeEntry === "UPPER CASE AND NUMERIC") {
       characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     } else if (charTypeEntry === "special and lower case" || charTypeEntry === "SPECIAL AND LOWER CASE" || charTypeEntry === "lower case and special" || charTypeEntry === "LOWER CASE AND SPECIAL") {
-      characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()"
+      characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+    } else if (charTypeEntry === "numeric and lower case" || charTypeEntry === "NUMERIC AND LOWER CASE" || charTypeEntry === "lower case and numeric" || charTypeEntry === "LOWER CASE AND NUMERIC") {
+      characters = "0123456789abcdefghijklmnopqrstuvwxyz";
+    } else if (charTypeEntry === "upper case and lower case" || charTypeEntry === "UPPER CASE AND LOWER CASE" || charTypeEntry === "lower case and upper case" || charTypeEntry === "LOWER CASE AND UPPER CASE") {
+      characters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqqrRsStTuUvVwWxXyYzZ";
+    } else if (charTypeEntry === "numeric and special" || charTypeEntry === "NUMERIC AND SPECIAL" || charTypeEntry === "special and numeric" || charTypeEntry === "SPECIAL AND NUMERIC") {
+      characters = "0123456789!@#$%^&*()";
     }
     //up to here works
     //return value
@@ -61,7 +67,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
