113eb7ad7599ae6c5067996795837bef56d0f9b6
londonlast21 <biffisbuffm@gmail.com>
2020-06-13 21:40:15 -0500

    concatenate strings for char

--- friendly-parakeet-master/Develop/script.js
+++ friendly-parakeet-master/Develop/script.js
@@ -8,53 +8,88 @@
 //when all prompts answered, password is generated that matches criteria
 //password is printed on screen
 
+
 //with each prompt entry user entry is validated and at least one char type is selecte
- function generatePassword() {
-   //ask for length when button clicked
-  var length = Number(prompt("Please enter number of characters, from 8 to 128."));
+function generatePassword() {
+  //ask for length when button clicked
+
+//  // var length = Number(prompt("Please enter number of characters, from 8 to 128."));
+
+//   while (isNaN(length) || length < 8 || length > 128 || length === "" || length === null) {
+
+//     length = Number(prompt("Invalid entry. Please enter number from 8 to 128."));
+
+//   }
+  
+  
+//   window.alert("You have chosen " + length + " characters.");
+  
+  
+
+
+
   //user validation
 
 
   //ask for character type
   var charType = prompt("Enter a character type: 'special', 'numeric', 'lower case', or 'upper case'. If choosing two, use the word 'and' and include entire term .");
+
   //generate the password from user input
-    var characters = "";
-    var charTypeEntry = charType.toLowerCase();
-    if (charTypeEntry === "lower case") {
+  var characters = "";
+  var charTypeEntry = charType.toLowerCase();
+  console.log("CHAR TYPES SELECTED STRING: ", charTypeEntry)
 
-      characters = "abcdefghijklmnopqrstuvwxyz";
+  let charSetSelected = false
 
-    } else if(charTypeEntry === "upper case") {
-      characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    } else if(charTypeEntry === "numeric") {
-      characters = "0123456789";
-    } else if (charTypeEntry === "special") {
-      characters = "!@#$%^&*()";
-    } else if (charTypeEntry === "special and upper case" || charTypeEntry === "upper case and special") {
-      characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
-    } else if (charTypeEntry === "numeric and upper case" || charTypeEntry === "upper case and numeric") {
-      characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    } else if (charTypeEntry === "special and lower case" || charTypeEntry === "lower case and special") {
-      characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
-    } else if (charTypeEntry === "numeric and lower case" || charTypeEntry === "lower case and numeric") {
-      characters = "0123456789abcdefghijklmnopqrstuvwxyz";
-    } else if (charTypeEntry === "upper case and lower case" || charTypeEntry === "lower case and upper case") {
-      characters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqqrRsStTuUvVwWxXyYzZ";
-    } else if (charTypeEntry === "numeric and special" || charTypeEntry === "special and numeric") {
-      characters = "0123456789!@#$%^&*()";
-    }
-    //up to here works
-    //return value
-    var returnValue = "";
-    for (var i = 0; i < length; i++) {
-      //picks a character set within the charType index
-      returnValue += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return returnValue;
+  if (charTypeEntry.includes("lower case")) {
 
-    
-    
-    }
+    characters = characters + "abcdefghijklmnopqrstuvwxyz";
+    charSetSelected = true;
+  }
+
+  if (charTypeEntry.includes("upper case")) {
+    characters = characters + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    charSetSelected = true;
+  }
+
+  if (charTypeEntry.includes("numeric")) {
+
+    characters = characters + "0123456789";
+    charSetSelected = true;
+
+  }
+
+  if (charTypeEntry.includes("special")) {
+
+    characters = characters + "!@#$%^&*()";
+    charSetSelected = true;
+
+  }
+
+  // while (!charSetSelected){
+  //   //charType = prompt("Invalid Entry! Enter a character type: 'special', 'numeric', 'lower case', or 'upper case'. If choosing two, use the word 'and' and include entire term .")
+  // }
+
+
+
+
+  console.log("HERES YOUR FINAL SET OF CHARS BUD: ",characters)
+
+
+  //up to here works
+  //return value
+  var returnValue = "";
+
+
+  for (var i = 0; i < length; i++) {
+    //picks a character set within the charType index
+    returnValue += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return returnValue;
+
+
+
+}
 
 
 
